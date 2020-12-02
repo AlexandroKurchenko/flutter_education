2a952d0c0bc05800e64040540b478c25a837fed1
Александр Курченко <aleksandrkurchenko@gmail.com>
2020-12-02 17:04:28 +0200

    intermediate commit with simple list

--- lib/ui/home/screens/coach_screen.dart
+++ lib/ui/home/screens/coach_screen.dart
@@ -17,12 +17,46 @@ class _CoachScreenState extends State<CoachScreen> {
     super.initState();
   }
 
+  List<String> _getListItems() {
+    List<String> listData = <String>[];
+    for (int i = 0; i < 123; i++) {
+      listData.add("Some input text fot index $i");
+    }
+    return listData;
+  }
+
+  List<Widget> _prepareListTitles() {
+    List<Widget> listData = <Widget>[];
+    _getListItems().forEach((element) {
+      listData.add(_wrapElement(element));
+    });
+    return listData;
+  }
+
+  Widget _wrapElement(String element) {
+    return Card(
+        child: ListTile(
+      title: Text(element),
+      subtitle: Text("Subtitle"),
+      onTap: () {
+        _showMeIndex(element);
+      },
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Hello user: " + block.userLogin.password + " on CoachScreen."),
+        body: Scrollbar(
+      child: ListView(
+        restorationId: "some_id",
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        children: _prepareListTitles(),
       ),
-    );
+    ));
+  }
+
+  _showMeIndex(String element) {
+    print("index is $element");
   }
 }
